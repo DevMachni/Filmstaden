cc05664ac2f850e458b2d7e5d779beb32157398e
Fredrik <fredrikbergstrand90@gmail.com>
2020-04-08 09:35:59 +0200

    Add React in Startup

--- Filmstaden.Website/Startup.cs
+++ Filmstaden.Website/Startup.cs
@@ -11,6 +11,10 @@ using Filmstaden.Website.Models;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Http;
+using JavaScriptEngineSwitcher.V8;
+using React.AspNet;
+using JavaScriptEngineSwitcher.Extensions.MsDependencyInjection;
 
 namespace Filmstaden.Website
 {
@@ -39,6 +43,13 @@ namespace Filmstaden.Website
             services.AddAuthentication()
                 .AddIdentityServerJwt();
 
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddReact();
+
+            // Make sure a JS engine is registered, or you will get an error!
+            services.AddJsEngineSwitcher(options => options.DefaultEngineName = V8JsEngine.EngineName)
+              .AddV8();
+
             services.AddControllersWithViews();
             services.AddRazorPages();
 
@@ -65,6 +76,27 @@ namespace Filmstaden.Website
             }
 
             app.UseHttpsRedirection();
+
+            // Initialise ReactJS.NET. Must be before static files.
+            app.UseReact(config =>
+            {
+                // If you want to use server-side rendering of React components,
+                // add all the necessary JavaScript files here. This includes
+                // your components as well as all of their dependencies.
+                // See http://reactjs.net/ for more information. Example:
+                //config
+                //  .AddScript("~/js/First.jsx")
+                //  .AddScript("~/js/Second.jsx");
+
+                // If you use an external build too (for example, Babel, Webpack,
+                // Browserify or Gulp), you can improve performance by disabling
+                // ReactJS.NET's version of Babel and loading the pre-transpiled
+                // scripts. Example:
+                //config
+                //  .SetLoadBabel(false)
+                //  .AddScriptWithoutTransform("~/js/bundle.server.js");
+            });
+
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
 
